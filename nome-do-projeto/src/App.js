b44282787c13d69ea6d5488e9e4f3c89874ed767
Eduardo Araujo Mantovani <117936343+dudumanto@users.noreply.github.com>
2024-02-20 13:29:28 -0300

    oi

--- nome-do-projeto/src/App.js
+++ nome-do-projeto/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to dois.
+          Edit <code>src/App.js</code> and save to dos.
         </p>
         <a
           className="App-link"
